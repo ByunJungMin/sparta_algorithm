016fc0c70383658bfcb5bb82e2ea25a734cdbb07
JungMin <cvy5375@naver.com>
2022-11-22 14:24:29 +0900

    숫자를 더하거나 곱해서 가장 큰 수 만들기

---
+++ week_1/05_find_max_plus_or_multiply.py
@@ -0,0 +1,18 @@
+# 배열안의 숫자를 더하거나 곱해서 가장 큰 수로 만들기
+
+input = [0, 3, 5, 6, 1, 2, 4]
+
+
+def find_max_plus_or_multiply(array):
+    multiply_sum = 0
+    for number in array:
+        if number <= 1 or multiply_sum <= 1:
+            multiply_sum += number
+        else:
+            multiply_sum *= number
+
+    return multiply_sum
+
+
+result = find_max_plus_or_multiply(input)
+print(result)
