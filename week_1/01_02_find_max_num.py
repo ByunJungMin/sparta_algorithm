0ca464daf09a3ea3954e61d1e7511462137a64ed
JungMin <cvy5375@naver.com>
2022-11-22 10:16:13 +0900

    최대값구하기1

--- week_1/01_02_find_max_num.py
+++ week_1/01_02_find_max_num.py
@@ -2,14 +2,15 @@
 
 input = [3, 5, 6, 1, 2, 4]
 
-def find_max_num(array):
-    for num in array:
-        for compare_num in array:
-            if num < compare_num:
-                break
 
-        else:
-            return num
+def find_max_num(array):
+    max_num = array[0]
+    for num in array:
+        if num > max_num:
+            max_num = num
+    
+    return max_num
+    
 
 
 result = find_max_num(input)
