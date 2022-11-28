98f884b5f573507ad4362a090084b7d2234ca7f5
JungMin <cvy5375@naver.com>
2022-11-28 11:04:03 +0900

    병합 정렬

---
+++ week_3/05_merge_sort.py
@@ -0,0 +1,40 @@
+# 병합 정렬
+
+array = [5, 3, 2, 1, 6, 8, 7, 4]
+
+
+def merge_sort(array):
+    if len(array) <= 1:                         # array 길이가 1 보다 작거나 같으면
+        return array                            # array 반환
+    mid = len(array) // 2                       # array 중간길이를 mid 에 저장
+    left_array = merge_sort(array[:mid])        # left_array 에 0부터 mid까지 저장
+    right_array = merge_sort(array[mid:])       # right_array 에 mid 부터 끝까지
+    return merge(left_array, right_array)       # left_array 와 right_array 를 병합하면서 정렬
+
+
+def merge(array1, array2):
+    result = []
+    array1_index = 0
+    array2_index = 0
+    while array1_index < len(array1) and array2_index < len(array2):
+        if array1[array1_index] < array2[array2_index]:
+            result.append(array1[array1_index])
+            array1_index += 1
+        else:
+            result.append(array2[array2_index])
+            array2_index += 1
+
+    if array1_index == len(array1):
+        while array2_index < len(array2):
+            result.append(array2[array2_index])
+            array2_index += 1
+
+    if array2_index == len(array2):
+        while array1_index < len(array1):
+            result.append(array1[array1_index])
+            array1_index += 1
+
+    return result
+
+
+print(merge_sort(array))  # [1, 2, 3, 4, 5, 6, 7, 8] 가 되어야 합니다!
